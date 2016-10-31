71afa8ee6bddf2bc211fb0a4c1f9b9c09b85d003
Jan Dočkal <dockal07@gmail.com>
2016-10-31 21:10:30 +0100

    init - get event from api

--- frontend/src/pages/EventDetailPage.js
+++ frontend/src/pages/EventDetailPage.js
@@ -2,13 +2,39 @@
  * Created by Honza on 22.10.2016.
  */
 import React, {Component} from 'react';
+import api from '../api.js';
 
 export class EventDetailPage extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      event: null
+    }
+  }
+
+  fetchEventDetailData() {
+    const {eventId} = this.props.params;
+    api('events/' + eventId)
+      .then((response)=> {
+        this.setState({event: response.data})
+      }).catch(()=> {
+
+    })
+  }
+
+  componentDidMount() {
+    this.fetchEventDetailData()
+  }
+
   render() {
-    const {event} = this.props.params
+    const {event} = this.state
     return (
       <div>
-        <h1>Detail of given event: {event} </h1>
+        {event == null ?
+          <h1>Načítám...</h1> :
+          <h1>Detail of given event: {event.name} </h1>
+        }
       </div>
     );
   }
