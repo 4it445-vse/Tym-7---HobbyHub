d7a27b5d70db6134e7c54d0a6109763e2d7ab6e3
Dominik Firla <dominik.firla@gmail.com>
2016-11-02 13:51:29 +0100

    aesthetic changes for event list and detail;

--- frontend/src/pages/EventDetailPage.js
+++ frontend/src/pages/EventDetailPage.js
@@ -34,23 +34,39 @@ export class EventDetailPage extends Component {
     const {event} = this.state
     return (
       <div className="container content-container">
+      
         {event == null ?
           <h1>Načítám...</h1> :
+
           <div>
-            <h1>{event.name}</h1>
+            <div className="col-md-12">
+              <h2>{event.name}</h2>
+            </div>
+
             {event.picture !==null?
-              <img src={event.picture}/>:
+              <img className="col-md-3" src={'/' + process.env.PUBLIC_URL + 'images/tenis.jpg'} alt="{name}"/>:
+              //<img className="col-md-4" src={event.picture}/>:
               <ImageNotFound width="200" height="150" />
             }
-            <div>{event.description}</div>
-            <div>{moment(event.date).format("DD MMMM YYYY")}</div>
-            <div>
-              {event.tags.split(",").map((tag)=>
-                tag
-              )}
+
+            <div className="col-md-6"><b>Autor</b> Ferda</div>
+            <div className="col-md-6"><b>Datum</b> {moment(event.date).format("DD MMMM YYYY")}</div>
+            <div className="col-md-6"><b>Kapacita</b> 1 / 2</div>
+            <div className="col-md-4">
+              <label><b>Kategorie</b></label>
+              <div className="col-md-12">
+                <ul className="tag-cloud">
+                  {event.tags.split(",").map((tag)=>
+                    <li><a class="btn btn-xs btn-primary" href="#">{tag}</a></li>
+                  )}
+                  </ul>
+              </div>
             </div>
+            <div className="col-md-6">{event.description}</div>
+
           </div>
         }
+
       </div>
     );
   }
