45a13a4428ac8b83027035cd69d8ee22eddef1b9
Mickel <Mickelsson23@gmail.com>
2016-11-14 00:54:13 +0100

    Edit log button position

--- frontend/src/pages/Events/EventDetailPage.js
+++ frontend/src/pages/Events/EventDetailPage.js
@@ -74,7 +74,13 @@ export class EventDetailPage extends Component {
                 </div>
               </div>
             </div>
+            <div className="col-md-12">
+            </div>
+            <div className="col-md-5">
+            </div>
+            <div className="col-md-7">
             <EventSignIn eventId={event.id}/>
+            </div>
           </div>
         }
       </div>
