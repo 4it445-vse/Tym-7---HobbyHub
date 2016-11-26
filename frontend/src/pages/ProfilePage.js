1fc228aa8811937a8229e6a94d42cdaf13de1709
Adam <adambrousek@gmail.com>
2016-11-26 18:15:27 +0100

    button change

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -37,12 +37,11 @@ export class ProfilePage extends Component {
               <div className="col-md-6"><b>Heslo</b><input type="text" className="form-control" value=""></input></div>
               <div className="col-md-6"><b>Heslo znovu</b><input type="text" className="form-control" value=""></input></div>
 
-              <div className="col-md-6">
+              <div className="col-md-12">
                   <button type="button" name="cancel" className="btn btn-danger btn-lg" required="required">Smazat profil</button>
-              </div>
-              <div className="col-md-6">
                   <button type="submit" name="submit" className="pull-right btn btn-success btn-lg" required="required">Uložit</button>
               </div>
+
 
 
 
