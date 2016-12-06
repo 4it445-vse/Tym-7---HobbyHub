5e5a5e9c340f943c839c81f57c5b583ef0b123ef
matha.lubos <matha.lubos@gmail.com>
2016-12-06 18:26:32 +0100

    user can add even only if they are logged in; if not logged in button add event is hidden and form for event add is replaced by log in! page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -6,7 +6,7 @@ import api from '../api.js';
 import {EventList} from '../components/EventList/EventList.js';
 import {connect} from 'react-redux';
 import {Link} from 'react-router';
-import {getUserId} from '../components/Login/reducers.js';
+import {getUserId, isLoggedIn} from '../components/Login/reducers.js';
 import {Filter} from '../components/Filter/Filter.js';
 
 export class HomePageRaw extends Component {
@@ -31,7 +31,7 @@ export class HomePageRaw extends Component {
 
   render() {
     const {events} = this.state;
-    const {userId} = this.props;
+    const {userId, isLoggedIn} = this.props;
 
     return (
       <div className="container content-container">
@@ -39,9 +39,9 @@ export class HomePageRaw extends Component {
             <div className="col-xs-8 col-md-8">
               <h2 className="pull-left">Události</h2>
             </div>
-            <div className="col-xs-4 col-md-4">
+          {isLoggedIn && <div className="col-xs-4 col-md-4">
               <Link className="pull-right btn btn-default top-buffer" to="/events/add">Vytvořit událost</Link>
-            </div>
+            </div>}
         </div>
           <div className="row  filter">
             <Filter/>
@@ -68,7 +68,8 @@ export class HomePageRaw extends Component {
 function mapStateToProps(state) {
   const {login} = state;
   return {
-    userId: getUserId(login)
+    userId: getUserId(login),
+    isLoggedIn: isLoggedIn(login)
   };
 }
 
