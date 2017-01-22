280146f754de1aaca341a3122cf4eecc3f449e02
Adam <adambrousek@gmail.com>
2017-01-22 11:56:07 +0100

    datepicker divided into future date selector / all dates selector

--- frontend/src/components/DatePicker/CustomDatePicker.js
+++ frontend/src/components/DatePicker/CustomDatePicker.js
@@ -42,6 +42,48 @@ export class CustomDatePicker extends Component {
       onChange={this.handleChange}
       dateFormat="YYYY/MM/DD"
       className="form-control"
+    />;
+  }
+}
+
+export class CustomDatePickerFuture extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentWillMount() {
+    this.setState({
+      startDate: this.props.startDate
+    })
+  }
+
+  /**
+   Updates state when value in datepicker changes.
+   */
+  handleChange(date) {
+    if (date.isValid() === true) {
+      this.setState({
+        startDate: date
+      });
+    } else {
+      this.setState({
+        startDate: null
+      });
+    }
+
+  }
+
+  render() {
+    const {id, name} = this.props;
+    return <DatePicker
+      todayButton={"Dnes"}
+      id={id}
+      name={name}
+      selected={this.state.startDate}
+      onChange={this.handleChange}
+      dateFormat="YYYY/MM/DD"
+      className="form-control"
       minDate={moment()}
     />;
   }
